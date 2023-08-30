d2d14373e73a8943d980430df0eff0fb5c07043a
Jiten Kumar Mohanty <jitenmohantyaz@gmail.com>
2023-08-30 10:56:17 +0530

    Contact FormGood to go

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import Header from "./components/Header";
 import Home from "./components/Home";
 import Work from "./components/Work";
 import Experince from "./components/Experince";
-import Services from "./components/Services.jsx";
-import Testimonial from "./components/Testimonial.jsx";
-import Contact from "./components/Contact.jsx";
+import Services from "./components/Services";
+import Testimonial from "./components/Testimonial";
+import Contact from "./components/Contact";
 import './styles/app.scss';
 import './styles/header.scss';
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
-     <Header/>
-     <Home/>
-     <Work/>
-     <Experince/>
-     <Services/>
-     <Testimonial/>
-     <Contact/>
+      <Header />
+      <Home />
+      <Work />
+      <Experince />
+      <Services />
+      <Testimonial />
+      <Contact />
+      <Toaster/>
     </>
   );
 }
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import vg from '../assets/vg.png';
+import {toast} from 'react-hot-toast';
 
 const Contact = () => {
+
+    const [name , setName] = useState("")
+    const [email , setEmail] = useState("")
+    const [message , setMessage] = useState("")
+
+const submitHandler = (e)=>{
+    e.preventDefault();
+    console.log(name,email,message);
+    toast.success('Message Sent')
+}
+
   return (
-    <div id='#contact'>
+    <div id="contact">
         <section>
-            <form action="">
+            <form action="" onSubmit={submitHandler}>
                 <h2>Contact Me</h2>
-                    
+                <input type="text" value={name} onChange={(e)=>setName(e.target.value)} placeholder='Your Name' required/>    
+                <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder='Your Email' required/>    
+                <input type="text" value={message} onChange={(e)=>setMessage(e.target.value)} placeholder='Your Message' required/>    
+
+                <button type='submit'>Send</button>
             </form>
         </section>
+        <aside>
+            <img src={vg} alt="img" />
+        </aside>
     </div>
   )
 }
 
-export default Contact
+export default Contact;
